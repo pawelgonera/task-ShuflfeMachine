639ab05abd944bc32cc1a4fbf8089609172032c4
poul124 <poulg12@gmail.com>
2018-10-04 17:37:30 +0200

    Implement create user method in Msain class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,10 @@ import java.util.*;
 
 public class Main
 {
+    static Scanner sc  = new Scanner(System.in);
+
     public static void main(String[] args)
     {
-        Scanner sc  = new Scanner(System.in);
 
         System.out.println("Podaj pełną ściężkę do pliku csv, razem z naswą pliku i rozszerzeniem .csv");
         String filename = sc.next();
@@ -27,7 +28,24 @@ public class Main
         System.out.println(Arrays.toString((shuffleMachine.drawOfWinners())));
 
 
+    }
 
+    public void createUser()
+    {
+        String name, lastName;
+        int age;
+
+        System.out.println("Type a name");
+        name = sc.next();
+
+        System.out.println("Type a lastName");
+        lastName = sc.next();
+
+        System.out.println("Type your age");
+        age = sc.nextInt();
+
+        User user = new User(name, lastName, age);
+        System.out.println("Created new user: " + user.toString());
 
     }
 }
