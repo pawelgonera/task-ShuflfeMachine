93e0be5e4121e199520e0ba43e1751a3fe34c532
poul124 <poulg12@gmail.com>
2018-10-04 17:58:47 +0200

    Implement method calculate in Main class
    Calculate method use Calculator class to calculating basic math operations

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,11 @@ import java.util.*;
 
 public class Main
 {
+    static Scanner sc  = new Scanner(System.in);
     public static void main(String[] args)
     {
-        Scanner sc  = new Scanner(System.in);
+
+        calculate();
 
         System.out.println("Podaj pełną ściężkę do pliku csv, razem z naswą pliku i rozszerzeniem .csv");
         String filename = sc.next();
@@ -27,7 +29,21 @@ public class Main
         System.out.println(Arrays.toString((shuffleMachine.drawOfWinners())));
 
 
+    }
 
+    public static void calculate()
+    {
+        Integer x, y;
+
+        System.out.println("Type first number");
+        x = sc.nextInt();
+        System.out.println("Type second number");
+        y = sc.nextInt();
+
+        System.out.println("Sum: " + Calculator.add(x, y));
+        System.out.println("Subtract: " + Calculator.subtract(x, y));
+        System.out.println("Multiply: " + Calculator.multiply(x, y));
+        System.out.println("Divide: " + Calculator.divide(x, y));
 
     }
 }
